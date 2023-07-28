d9c1a6aa4f20ce2ed34f43083b3a2f5ab2041497
Tushar Nain <naintushar@hotmail.com>
2023-07-28 15:23:22 +0530

    added options in database connection

--- db.js
+++ db.js
@@ -2,6 +2,9 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 mongoose
-  .connect(process.env.MONGODB_URI)
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("Database Connected"))
   .catch((err) => console.log(err));
