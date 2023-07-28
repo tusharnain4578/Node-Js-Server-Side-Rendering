b54ff7ec4d2e992472b4822c8d5fe935b783de8b
Tushar Nain <naintushar@hotmail.com>
2023-07-28 15:26:20 +0530

    fixed database connection

--- db.js
+++ db.js
@@ -2,9 +2,6 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log("Database Connected"))
   .catch((err) => console.log(err));
